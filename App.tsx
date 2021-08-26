eba8dad8841458035061d5d7584e0357eb678f2f
Rafael Lessa <rafael.lessa150@gmail.com>
2021-08-26 17:53:57 -0300

    feat: up project

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <Text
+      style={{
+        margin: 100,
+        fontFamily: 'PTSans-Regular',
+        fontSize: 24,
+      }}>
+      Olá Amigo
+    </Text>
+  );
+};
+
+export default App;
