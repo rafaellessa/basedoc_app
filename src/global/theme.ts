ef0a00b5c50de874c0d955604e41e8428dbac456
Rafael Lessa <rafael.lessa150@gmail.com>
2021-08-26 18:03:37 -0300

    feat: create theme style

---
+++ src/global/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    primary: '#5AA494',
+    secondary: '#A8D3AF',
+    white: '#fff',
+  },
+  fonts: {
+    regular: 'PTSans-Regular',
+    bold: 'PTSans-Bold',
+  },
+};
