f52afd3ae8b3a8323b365de939418da33cd859b2
Rafael Lessa <rafael.lessa150@gmail.com>
2021-08-26 21:22:29 -0300

    chore: config reactoTron and redux

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Stack from './src/navigations/Stack';
 
 const App: React.FC = () => {
+  if (__DEV__) {
+    import('./ReactoTron').then(() => console.log('Reactotron Configurado'));
+  }
   return (
     <NavigationContainer>
       <Stack />
