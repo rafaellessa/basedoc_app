bbbe4d3ad85e578f597b650c5a87ddd3f037a924
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-28 23:52:13 -0300

    feat: implement store provider in app

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {Provider} from 'react-redux';
 import Stack from './src/navigations/Stack';
+import store from './src/redux/store';
 
 const App: React.FC = () => {
   if (__DEV__) {
     import('./ReactoTron').then(() => console.log('Reactotron Configurado'));
   }
   return (
-    <NavigationContainer>
-      <Stack />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
