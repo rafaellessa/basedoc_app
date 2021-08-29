b6a1ce325a9febd33377d7f93f0de729f1faddf9
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-28 23:53:10 -0300

    feat: implement redux

--- src/redux/reducers/reducer.administrators.ts
+++ src/redux/reducers/reducer.administrators.ts
@@ -1,55 +1,56 @@
-import { createActions, createReducer } from 'reduxsauce';
+import {createActions, createReducer} from 'reduxsauce';
 import Immutable from 'seamless-immutable';
 import {
-  AdministratorActionTypes, CreatorTtypes, ReducerTypes
-} from '../types/types.administrators';
-import { AdministratorState } from './../types/types.administrators';
-import { BaseDocReducers } from './../types/types.redux';
-
-
-const {Types, Creators} = createActions<
   AdministratorActionTypes,
-  CreatorTtypes
->({
-  basedocRequestGetAllAdministrators: [],
-  basedocSuccessGetAllAdministrators: ['administrators'],
-  basedocFailureGetAllAdministrators: ['error'],
-  reset: null,
-},{
-  prefix: `${BaseDocReducers.root}/${BaseDocReducers.administrators}`;
-});
+  AdministratorState,
+  ReducerTypes,
+} from '../types/types.administrators';
+import {BaseDocReducers} from '../types/types.redux';
+import {CreatorTypes} from './../types/types.administrators';
+
+const {Types, Creators} = createActions<AdministratorActionTypes, CreatorTypes>(
+  {
+    basedocRequestGetAllAdministrators: [],
+    basedocSuccessGetAllAdministrators: ['administrators'],
+    basedocFailureGetAllAdministrators: ['error'],
+    reset: null,
+  },
+  {
+    prefix: `${BaseDocReducers.root}/${BaseDocReducers.administrators}`,
+  },
+);
 
 const INITIAL_STATE: AdministratorState = Immutable({
   administrators: [],
   loading: false,
-  error: null
-})
+  error: null,
+});
 
 export const reducer = createReducer<AdministratorState, ReducerTypes>(
   INITIAL_STATE,
   {
-    [Types.BASEDOC_REQUEST_GET_ALL_ADDMINISTRATORS]: (state) => {
+    [Types.BASEDOC_REQUEST_GET_ALL_ADMINISTRATORS]: state => {
       return state.merge({
         loading: true,
-        error: null
-      })
+        error: null,
+      });
     },
-    [Types.BASEDOC_SUCCESS_GET_ALL_ADDMINISTRATORS]: (state, action) => {
+    [Types.BASEDOC_SUCCESS_GET_ALL_ADMINISTRATORS]: (state, action) => {
       return state.merge({
         loading: false,
         administrators: action.administrators,
-        error: null
-      })
+        error: null,
+      });
     },
-    [Types.BASEDOC_FAILURE_GET_ALL_ADDMINISTRATORS]: (state, action) => {
+    [Types.BASEDOC_FAILURE_GET_ALL_ADMINISTRATORS]: (state, action) => {
       return state.merge({
         loading: false,
-        error: action.error
-      })
+        error: action.error,
+      });
     },
-    [Types.RESET]: () => INITIAL_STATE
-  }
-)
+    [Types.RESET]: () => INITIAL_STATE,
+  },
+);
 
-export const AdministratorActions = Creators
-export const AdministratorTypes = Types
+export const AdministratorActions = Creators;
+export const AdministratorTypes = Types;
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,4 +1,26 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import ReactotronConfig from '../../../ReactoTron';
-import {reducer as reducerAdministrators} from '../reducers/reducer.administrators';
+import {reducer as reducerAdministrator} from '../reducers/reducer.administrators';
+import rootSaga from './../sagas/sagas.administrators';
+
+const sagaMonitor = ReactotronConfig.createSagaMonitor?.();
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
+
+const composer = compose(
+  applyMiddleware(sagaMiddleware),
+  ReactotronConfig.createEnhancer(),
+);
+
+const store = createStore(
+  combineReducers({
+    reducerAdministrator,
+  }),
+  composer,
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
