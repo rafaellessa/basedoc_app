51750cc40aa46094aa4d30b8eddc368d2a8145d8
Rafael Lessa <rafael.lessa@basesoft.com.br>
2021-08-28 23:07:50 -0300

    feat: implements api service datasource

---
+++ src/data/datasource/api/index.ts
@@ -0,0 +1,16 @@
+import axios, {AxiosInstance} from 'axios';
+
+export default class Api {
+  private static apiInstance: AxiosInstance;
+  public static getApiInstance(baseUrl: string): AxiosInstance {
+    const api = axios.create({
+      baseURL: baseUrl,
+      timeout: 1200,
+      //headers: {Authorization: 'Bearer ' + token},,
+    });
+    this.apiInstance = api;
+    return this.apiInstance;
+  }
+}
+
+
